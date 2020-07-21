aaf8f9979afa532b4e0eaf1d41b9b523841f7462
Tanay Chitre <tanaychitre23@gmail.com>
2020-07-21 19:43:17 +0530

    Create problem2_tanay.py
    https://projecteuler.net/problem=2
    
    
    
    Each new term in the Fibonacci sequence is generated by adding the previous two terms. By starting with 1 and 2, the first 10 terms will be:
    
    1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
    
    By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.

---
+++ problem2_tanay.py
@@ -0,0 +1,21 @@
+
+memo ={}
+def fastFib(n,memo):
+    """Assumes n is an int >= 0, memo used only by recursive calls
+       Returns Fibonacci of n"""
+    if n == 0 or n == 1:
+        return 1
+    try:
+        return memo[n]
+    except KeyError:
+        result = fastFib(n-1, memo) + fastFib(n-2, memo)
+        memo[n] = result
+        return result
+    
+fastFib(8,memo)
+sumfib = 0 
+for item in memo.values():
+    if item%2 == 0:
+        sumfib += item
+
+print(sumfib)
